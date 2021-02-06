d837ebf5c66ea3e2bbf5de176bcd4042b1f0e915
SeHun Shin <s.shin@marathoner.kr>
2021-02-07 00:05:26 +0900

    add adSense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,7 @@ class MyDocument extends Document {
     return (
       <Html lang="ko">
         <Head>
+          
           <script data-ad-client="ca-pub-9646115072766415" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>        </Head>
         <body>
           <Main />
